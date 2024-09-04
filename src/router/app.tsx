4a6e987ee4d386e6f8c071ef43c770730a7ad32a
thetiagogil <talmeidagil@hotmail.com>
2024-09-04 19:33:27 +0100

    remove sidebar component and dashboard and overview pages

--- src/router/app.tsx
+++ src/router/app.tsx
@@ -2,10 +2,8 @@ import { useContext } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AuthContext } from "../contexts/auth.context";
 import { ActivityPage } from "../pages/activity.page";
-import { DashboardPage } from "../pages/dashboard.page";
 import { HomePage } from "../pages/home.page";
 import { LoginPage } from "../pages/login.page";
-import { OverviewPage } from "../pages/overview.page";
 import { SignupPage } from "../pages/signup.page";
 
 export const App = () => {
@@ -21,10 +19,8 @@ export const App = () => {
         </Routes>
       ) : hasData ? (
         <Routes>
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/overview" element={<OverviewPage />} />
           <Route path="/activity" element={<ActivityPage />} />
-          <Route path="*" element={<Navigate to="/dashboard" />} />
+          <Route path="*" element={<Navigate to="/activity" />} />
         </Routes>
       ) : (
         <Routes>
