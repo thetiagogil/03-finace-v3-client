4e018fb2a3dcc004e835afa8cae10f26953490cd
thetiagogil <talmeidagil@hotmail.com>
2024-09-06 17:38:18 +0100

    update tabs navbar to stay fixed

--- src/components/shared/tabs-status.tsx
+++ src/components/shared/tabs-status.tsx
@@ -8,13 +8,21 @@ type TabsStatusProps = {
 };
 
 export const TabsStatus = ({ trackedTab, plannedTab }: TabsStatusProps) => {
-  const stylesTabPanel = { m: 0, p: 0, width: MAIN_WIDTH, alignSelf: "center" };
+  const stylesTabPanel = {
+    m: 0,
+    py: 0,
+    px: { xs: 2, lg: 0 },
+    width: { xs: "100%", lg: MAIN_WIDTH },
+    alignSelf: "center"
+  };
   return (
     <Tabs
       defaultValue={0}
       sx={{
         bgcolor: MAIN_BGCOLOR,
         borderRadius: MAIN_BORDER_RADIUS,
+        height: "100vh",
+        overflow: "hidden",
         [`& .${tabClasses.root}`]: {
           color: "neutral.400",
           alignSelf: "center",
@@ -35,19 +43,34 @@ export const TabsStatus = ({ trackedTab, plannedTab }: TabsStatusProps) => {
         }
       }}
     >
-      <Flex y sx={{ mb: 4 }}>
+      <Flex y>
         <TabList disableUnderline sx={{ height: NAVBAR_HEIGHT, width: "100%", justifyContent: "center" }}>
           <Tab disableIndicator>Tracked</Tab>
           <Tab disableIndicator>Planned</Tab>
         </TabList>
         <Divider orientation="horizontal" />
       </Flex>
-      <TabPanel value={0} sx={stylesTabPanel}>
-        {trackedTab}
-      </TabPanel>
-      <TabPanel value={1} sx={stylesTabPanel}>
-        {plannedTab}
-      </TabPanel>
+      <Flex x fullwidth sx={{ height: `calc(100vh - ${NAVBAR_HEIGHT}px)`, overflow: "hidden", mb: 2 }}>
+        <Flex
+          y
+          xc
+          component="main"
+          sx={{
+            flex: 1,
+            overflowY: "auto",
+            height: "100%"
+          }}
+        >
+          <Flex y sx={{ height: "100%", width: { xs: "100%", lg: MAIN_WIDTH }, mt: 4 }}>
+            <TabPanel value={0} sx={stylesTabPanel}>
+              {trackedTab}
+            </TabPanel>
+            <TabPanel value={1} sx={stylesTabPanel}>
+              {plannedTab}
+            </TabPanel>
+          </Flex>
+        </Flex>
+      </Flex>
     </Tabs>
   );
 };
