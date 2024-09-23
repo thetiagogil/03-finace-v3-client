0f3e83f00ac7b09a79156fc4205dc7c14a43a0a2
thetiagogil <talmeidagil@hotmail.com>
2024-09-23 16:05:30 +0100

    update budget page

--- src/components/layout/budget-column.tsx
+++ src/components/layout/budget-column.tsx
@@ -1,11 +1,64 @@
+import { LinearProgress, Typography } from "@mui/joy";
 import { ComponentTitle } from "../shared/component-title";
+import { DataCard } from "../shared/data-card";
 import { Flex } from "../shared/flex";
 
-export const BudgetColumn = () => {
+type BudgetColumnProps = {
+  categoriesSum: {
+    incomes: Record<string, { tracked: number; planned: number }>;
+    expenses: Record<string, { tracked: number; planned: number }>;
+  };
+};
+
+export const BudgetColumn = ({ categoriesSum }: BudgetColumnProps) => {
   return (
     <>
-      <ComponentTitle title="Budget" />
-      <Flex>Hello</Flex>
+      <ComponentTitle title="Budget Overview" />
+      <Flex gap2 sx={{ flexDirection: { xs: "column", md: "row" } }}>
+        {renderTransactionList("Income", categoriesSum.incomes, "success.500", "No income planned.")}
+        {renderTransactionList("Expenses", categoriesSum.expenses, "danger.500", "No expenses planned.")}
+      </Flex>
     </>
+  );
+};
+
+const renderTransactionList = (
+  title: string,
+  data: Record<string, { tracked: number; planned: number }>,
+  positiveColor: string,
+  noDataText: string
+) => {
+  const calculateProgress = (tracked: number, planned: number) => {
+    const percentage = (tracked / planned) * 100;
+    return planned > 0 ? (percentage < 100 ? percentage : 100) : 0;
+  };
+  return (
+    <Flex y fullwidth>
+      <DataCard>
+        <Typography level="title-sm" sx={{ mb: 2 }}>
+          {title}
+        </Typography>
+        <Flex y gap2 fullwidth>
+          {Object.keys(data).length > 0 ? (
+            Object.entries(data).map(([category, { tracked, planned }]) => {
+              const percentage = Number(((Math.abs(tracked) / Math.abs(planned)) * 100).toFixed(0));
+              return (
+                <Flex key={category} y gap1>
+                  <Flex x yc xsb>
+                    <Typography level="body-sm">{category}</Typography>
+                    <Typography level="body-sm" sx={{ color: percentage > 100 ? positiveColor : null }}>
+                      {percentage}%
+                    </Typography>
+                  </Flex>
+                  <LinearProgress determinate value={calculateProgress(tracked, planned)} />
+                </Flex>
+              );
+            })
+          ) : (
+            <Typography level="body-sm">{noDataText}</Typography>
+          )}
+        </Flex>
+      </DataCard>
+    </Flex>
   );
 };
--- src/pages/budget.page.tsx
+++ src/pages/budget.page.tsx
@@ -1,17 +1,27 @@
 import { useContext } from "react";
+import { useGetYearCategorySummary } from "../api/years-api";
 import { AddActivity } from "../components/layout/add-activity";
 import { BudgetColumn } from "../components/layout/budget-column";
 import { AuthPageContainer } from "../components/shared/containers";
 import { Flex } from "../components/shared/flex";
+import { Loading } from "../components/shared/loading";
 import { AuthContext } from "../contexts/auth.context";
+import { InfoContext } from "../contexts/info.context";
 
 export const BudgetPage = () => {
   const { userId } = useContext(AuthContext);
+  const { loadingUserData } = useContext(InfoContext);
+  const { data: categoriesSum, loading: categoriesSumLoading } = useGetYearCategorySummary({
+    userId,
+    year: 2024,
+    month: "feb"
+  });
+  const isLoading = loadingUserData || categoriesSumLoading;
   return (
     <AuthPageContainer
       leftChildren={
         <Flex y gap2>
-          <BudgetColumn />
+          {isLoading ? <Loading /> : <BudgetColumn categoriesSum={categoriesSum} />}
         </Flex>
       }
       rightChildren={
